9db080aefb7e1b2cc8550d16e5100ec8cf2c6720
vaibhav975 <vaibhavvishal277@gmail.com>
2022-10-22 14:49:11 +0530

    todo error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
+    TodoItemComponent,
   ],
   imports: [
     BrowserModule,
